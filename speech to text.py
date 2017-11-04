1b79c0b50cb93cd5a59f156489bb2682988579a7
Rshcaroline <rshcaroline@163.com>
2017-11-04 16:48:13 +0800

    baseline

--- speech to text.py
+++ speech to text.py
@@ -8,9 +8,45 @@
 
 import os
 import json
+import pyaudio
 import time
-import string
-import nltk
+import wave
+import multiprocessing
+from multiprocessing import Pool
+
+# PyAudio example: Record a few seconds of audio and save to a WAVE file
+def record(path):
+    CHUNK = 1024
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 2
+    RATE = 44100
+    RECORD_SECONDS = 5
+
+    WAVE_OUTPUT_FILENAME = path
+
+    p = pyaudio.PyAudio()
+    stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK)
+
+    print("* recording")
+    frames = []
+    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        data = stream.read(CHUNK)
+        frames.append(data)
+
+    print("* done recording")
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+    wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+    wf.writeframes(b''.join(frames))
+    wf.close()
 
 def detec(text):  # detec the id of player
     words = text.split()
@@ -18,9 +54,12 @@ def detec(text):  # detec the id of player
     for word in words:
         if word in dict: return dict[word]
 
+def print_ts(message):  # print headlines
+    print("[%s] %s"%(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), message))
+
 def get_speeches(path):  # get speeched to text
     cmd = 'curl -X POST -u 810e0500-12e4-4ed8-a1ac-4706ae4858d1:IjZZ80MAGJlY \
-    --header "Content-Type: audio/flac" \
+    --header "Content-Type: audio/wav" \
     --header "Transfer-Encoding: chunked" \
     --data-binary @' + path + ' \
     "https://stream.watsonplatform.net/speech-to-text/api/v1/recognize"'
@@ -29,12 +68,18 @@ def get_speeches(path):  # get speeched to text
     speech = json.loads(tmp)
     text = speech['results'][0]['alternatives'][0]['transcript']
 
+    print_ts("-" * 50)
+    print(text)
     # text = 'Number seven , He got it !'
-    return text
+    id = detec(text)
+    return text, id
 
-def main_loop():
-     path = '/Users/caroline/Downloads/audio-file.flac'
+def main_loop(i):
+     path = './' + str(i) + '.wav'
+     record(path)
      get_speeches(path)
 
-if __name__ == '__main__':
-    main_loop()
+
+if __name__ == '__main__' :
+  for i in range(1, 6):
+    main_loop(i)
